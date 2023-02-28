1694edbce2529ee45a8481a6fa4e298bded8efa5
Viscount <siyanov.d@sar.garm.ru>
2023-02-28 23:39:50 +0400

    task06 that's all(all)

--- src/main/java/task06/Calculator.java
+++ src/main/java/task06/Calculator.java
@@ -1,23 +1,43 @@
 package task06;
 
 public class Calculator {
+
     public static double totalArea(Figure [][] arrayFigures){
         double totalSquare = 0;
 
         for (int i = 0; i < arrayFigures.length; i++) {
 
-
-            for (int j = 0; j < arrayFigures.length; j++) {
-
-                totalSquare = totalSquare + arrayFigures[i][j].square();
-
-            }
+                totalSquare = totalSquare + Calculator.totalSquare(arrayFigures[i]) ;
 
         }
         return totalSquare;
 
     }
 
+    private static double totalSquare(Figure[] figures) {
+        double totalSquare = 0;
+        for (int i = 0; i < figures.length; i++) {
+            totalSquare = totalSquare + figures[i].square();
 
+        }
+        return totalSquare;
+    }
+
+//    public static double totalArea(Figure [][] arrayFigures){
+//        double totalSquare = 0;
+//
+//          for (int i = 0; i < arrayFigures.length; i++) {
+//
+//
+//            for (int j = 0; j < arrayFigures.length; j++) {
+//
+//                totalSquare = totalSquare + arrayFigures[i][j].square();
+//
+//            }
+//
+//        }
+//        return totalSquare;
+//
+//    }
 
 }
